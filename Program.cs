2a2cde017ffe2dee5c185a39758fe92a33aac88f
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-15 08:28:10 -0400

    Working!

--- Program.cs
+++ Program.cs
@@ -11,17 +11,17 @@ namespace SumOfElementsOfArray
         static void Main(string[] args)
         {
             int[] exampleArray = { 20, 10, 5, 2 };
-            Console.WriteLine("The sum of the example array is {}", SumNum(exampleArray));
-                                //How does {0} work?                    //This an equation multiplying the 2?
-            SumNum(exampleArray);
+            Console.WriteLine("The sum of the example array is " + SumNum(exampleArray));
+                                //How does {0} work?                    //SumNum is the method being applied to ExampleArray
+            SumNum(exampleArray);  //taking answer from next method and summing it in my array
 
         }
         static int SumNum(int[] exampleArray)
         {
             int answer = 0;
-            for(int i = 0; i < exampleArray.Length; i++)
+            for(int i = 0; i < exampleArray.Length; i++) //For loops often used with arrays
             {
-                Console.WriteLine(exampleArray[i]);
+                answer += exampleArray[i]; //This lines means variable of "answer" + exampleArray AT THE INDEX OF [i]
             }
         return answer;
 
