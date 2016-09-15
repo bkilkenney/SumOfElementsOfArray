97ab528e2965ca4a1284e9eb815ed9ff838b120f
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-15 08:02:02 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfElementsOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] exampleArray = { 20, 10, 5, 2 };
+            Console.WriteLine("The sum of the example array is {}", SumNum(exampleArray));
+                                //How does {0} work?                    //This an equation multiplying the 2?
+            SumNum(exampleArray);
+
+        }
+        static int SumNum(int[] exampleArray)
+        {
+            int answer = 0;
+            for(int i = 0; i < exampleArray.Length; i++)
+            {
+                Console.WriteLine(exampleArray[i]);
+            }
+        return answer;
+
+        }
+    }
+}
